521f7a9ccefc0a7b211d2133881375184e3f21d1
suresh Daneti <sureshdaneti99@gmail.com>
2021-09-17 15:28:22 +0530

    wellcome

---
+++ Wellcome.java
@@ -0,0 +1,12 @@
+package Com.Bridgelabs.Employeewage;
+
+public class Wellcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Programme");
+		
+		
+
+	}
+
+}
